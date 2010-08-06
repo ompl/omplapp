8039c7cfa04a8a786867277fe098cb4184000e73
Mark Moll <mmoll@rice.edu>
2010-08-06 17:00:28 +0000

    automatically download PQP, if necessary. small build system changes
    --HG--
    extra : convert_revision : 61378fa941b07402124651b69255d6e746f0c2b4

--- trunk/omplapp/CMakeModules/UseAssimp.cmake
+++ trunk/omplapp/CMakeModules/UseAssimp.cmake
@@ -1,7 +1,11 @@
 set(ASSIMP_ROOT_DIR "${CMAKE_SOURCE_DIR}/external/assimp")
 set(ASSIMP_INCLUDE_DIR "${ASSIMP_ROOT_DIR}/include")
+# the next three variables are defined in assimp's top-level CMakeLists.txt, but
+# we're skipping that one, since we only want the core assimp library and none 
+# of the other stuff.
 set(LIB_INSTALL_DIR "lib")
 set(INCLUDE_INSTALL_DIR "include")
 set(BIN_INSTALL_DIR "bin")
+
 add_subdirectory("${ASSIMP_ROOT_DIR}/code")
 set(ASSIMP_LIBRARY "assimp")
--- trunk/omplapp/CMakeModules/UsePQP.cmake
+++ trunk/omplapp/CMakeModules/UsePQP.cmake
@@ -1,3 +1,72 @@
+set(PQP_DIR "${CMAKE_SOURCE_DIR}/external/pqp-1.3")
+set(PQP_TGZ "${PQP_DIR}.tar.gz")
+# check if PQP directory already exists
+# if not, download and extract PQP
+if(NOT EXISTS ${PQP_DIR})
+	if(NOT EXISTS ${PQP_TGZ})
+ 		file(DOWNLOAD http://gamma.cs.unc.edu/software/downloads/SSV/pqp-1.3.tar.gz 
+			${PQP_TGZ} STATUS PQP_DL_STATUS)
+		# check download exit status
+		list(GET PQP_DL_STATUS 0 PQP_DL_STATUS_VAL)
+		list(GET PQP_DL_STATUS 1 PQP_DL_STATUS_MSG)
+		if(NOT PQP_DL_STATUS_VAL EQUAL 0)
+			message(FATAL_ERROR "PQP download failed with this error: ${PQP_DL_STATUS_MSG}")
+		endif()
+		# check MD5 checksum
+		execute_process(COMMAND ${CMAKE_COMMAND} -E md5sum ${PQP_TGZ}
+			OUTPUT_VARIABLE PQP_MD5)
+		if(NOT PQP_MD5 MATCHES "^f710e24a62db763d61d08667439d46fd.*pqp-1.3.tar.gz.*")
+			message(WARNING "The PQP distribution has changed. Compilation may fail.")
+		endif()
+	endif()
+	# extract PQP tar ball
+	execute_process(COMMAND ${CMAKE_COMMAND} -E tar xzf ${PQP_TGZ}
+		WORKING_DIRECTORY "${CMAKE_SOURCE_DIR}/external")
+	# show PQP license
+	message(STATUS "	
+-------------------------------------------------------------------------------
+Copyright 1999 The University of North Carolina at Chapel Hill.
+All Rights Reserved.
+
+Permission to use, copy, modify and distribute this software and its
+documentation for educational, research and non-profit purposes, without
+fee, and without a written agreement is hereby granted, provided that the
+above copyright notice and the following three paragraphs appear in all
+copies.
+
+IN NO EVENT SHALL THE UNIVERSITY OF NORTH CAROLINA AT CHAPEL HILL BE
+LIABLE TO ANY PARTY FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR
+CONSEQUENTIAL DAMAGES, INCLUDING LOST PROFITS, ARISING OUT OF THE
+USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF THE UNIVERSITY
+OF NORTH CAROLINA HAVE BEEN ADVISED OF THE POSSIBILITY OF SUCH
+DAMAGES.
+
+THE UNIVERSITY OF NORTH CAROLINA SPECIFICALLY DISCLAIM ANY
+WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.  THE SOFTWARE
+PROVIDED HEREUNDER IS ON AN \"AS IS\" BASIS, AND THE UNIVERSITY OF
+NORTH CAROLINA HAS NO OBLIGATIONS TO PROVIDE MAINTENANCE, SUPPORT,
+UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
+
+The authors may be contacted via:
+
+US Mail:             E. Larsen
+                     Department of Computer Science
+                     Sitterson Hall, CB #3175
+                     University of N. Carolina
+                     Chapel Hill, NC 27599-3175
+
+Phone:               (919)962-1749
+
+EMail:               geom@cs.unc.edu
+-------------------------------------------------------------------------------
+
+** THE PQP LIBRARY FOR COLLISION CHECKING HAS BEEN DOWNLOADED. BY USING THIS 
+** LIBRARY YOU AGREE TO THE LICENSE ABOVE.
+")
+endif()
+
+
 macro(ompl_backup filename)
 	if(NOT EXISTS "${filename}.orig")
 		if(${CMAKE_MAJOR_VERSION}.${CMAKE_MINOR_VERSION} VERSION_GREATER 2.6)
@@ -32,7 +101,8 @@ if(MSVC_IDE)
 	message(STATUS "Patched ${PQP_INCLUDE}/PQP_Internal.h")
 endif(MSVC_IDE)
 
-set(PQP_INCLUDE_DIR "${CMAKE_SOURCE_DIR}/external/PQP_v1.3/src")
+
+set(PQP_INCLUDE_DIR "${PQP_DIR}/PQP_v1.3/src")
 aux_source_directory(${PQP_INCLUDE_DIR} SRC_PQP)
 add_library(PQP SHARED ${SRC_PQP})
 set(PQP_LIBRARY "PQP")
