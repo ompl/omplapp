9d829263902d6b44efc1f819aaaff030abf6f57b
Mark Moll <mmoll@rice.edu>
2010-08-27 19:50:18 +0000

    add cpack settings
    --HG--
    extra : convert_revision : 82abd1142c3f049dad635456cabc2d460a4b9317

---
+++ trunk/omplapp/CMakeModules/CPackSettings.cmake
@@ -0,0 +1,16 @@
+set(CPACK_PACKAGE_DESCRIPTION_SUMMARY "The Open Motion Planning Library (OMPL) & front-end (OMPL-APP)")
+set(CPACK_PACKAGE_VENDOR "Rice University")
+set(CPACK_PACKAGE_CONTACT "Mark Moll <mmoll@rice.edu>")
+set(CPACK_RSRC_DIR "${CMAKE_CURRENT_SOURCE_DIR}/CMakeModules")
+
+set(CPACK_PACKAGE_VERSION_MAJOR "${OMPLAPP_MAJOR_VERSION}")
+set(CPACK_PACKAGE_VERSION_MINOR "${OMPLAPP_MINOR_VERSION}")
+set(CPACK_PACKAGE_VERSION_PATCH "${OMPLAPP_PATCH_VERSION}")
+
+set(CPACK_SOURCE_IGNORE_FILES 
+	"$/CVS/;/.svn/;.swp$;.#;/#;/build/;.pyc$;.pyo$;.so$;.dylib$;.md5$;/blueprint/;.DS_Store;_cache$;mkwebdocs.sh;ompl/doc/html;")
+set(CPACK_SOURCE_GENERATOR "TGZ;ZIP")
+set(CPACK_GENERATOR "TGZ;ZIP")
+
+include(CPack)
+
