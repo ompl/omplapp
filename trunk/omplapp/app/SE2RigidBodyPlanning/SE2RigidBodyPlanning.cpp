cd3488ff9504ed74e87a6cbe9264b1c9c6d09174
Devin Grady <devin.grady@rice.edu>
2010-09-23 19:34:18 +0000

    changed the SE2 and SE3 demo cpp programs to have valid start and goal states as well as load the correct environment files
    --HG--
    extra : convert_revision : a2378ff84e3dac90a4277c49c9d46aea0b56c0a6

--- trunk/omplapp/app/SE2RigidBodyPlanning/SE2RigidBodyPlanning.cpp
+++ trunk/omplapp/app/SE2RigidBodyPlanning/SE2RigidBodyPlanning.cpp
@@ -6,18 +6,18 @@ using namespace ompl;
 int main()
 {
     app::SE2RigidBodyPlanning setup;
-    std::string robot_fname = std::string(OMPL_RESOURCE_DIR) + "/Twistycool_robot.dae";
-    std::string env_fname = std::string(OMPL_RESOURCE_DIR) + "/Twistycool_env.dae";
+    std::string robot_fname = std::string(OMPL_RESOURCE_DIR) + "/Easy_robot.dae";
+    std::string env_fname = std::string(OMPL_RESOURCE_DIR) + "/Easy_env.dae";
     setup.setRobotMesh(robot_fname.c_str());
     setup.setEnvironmentMesh(env_fname.c_str());
     
-    base::ScopedState<> start(setup.getSpaceInformation());
-    start.random();
+    base::ScopedState<base::SE2StateManifold> start(setup.getSpaceInformation());
+    start->setX(270.40);
+    start->setY(-196.82);
     
     base::ScopedState<base::SE2StateManifold> goal(start);
-    goal->setYaw(0.0);
-    goal->setX(2.0);
-    goal->setY(3.0);
+    start->setX(270.40);
+    start->setY(-396.82);
     
     setup.setStartAndGoalStates(start, goal);
 
--- trunk/omplapp/app/SE3RigidBodyPlanning/SE3RigidBodyPlanning.cpp
+++ trunk/omplapp/app/SE3RigidBodyPlanning/SE3RigidBodyPlanning.cpp
@@ -5,25 +5,25 @@ using namespace ompl;
 int main()
 {
     app::SE3RigidBodyPlanning setup;
-    std::string robot_fname = std::string(OMPL_RESOURCE_DIR) + "/cubicles_robot.dae";
-    std::string env_fname = std::string(OMPL_RESOURCE_DIR) + "/cubicles_env.dae";
+    std::string robot_fname = std::string(OMPL_RESOURCE_DIR) + "/alpha_robot.dae";
+    std::string env_fname = std::string(OMPL_RESOURCE_DIR) + "/alpha_env.dae";
     setup.setRobotMesh(robot_fname.c_str());
     setup.setEnvironmentMesh(env_fname.c_str());
     
     base::ScopedState<base::SE3StateManifold> start(setup.getSpaceInformation());
-    start->setX(82.07);
-    start->setY(46.98);
-    start->setZ(316.01);
-    start->rotation().setAxisAngle(1, 0, 0, M_PI * -35.0/180.0);
+    start->setX(17.49);
+    start->setY(10.12);
+    start->setZ(-1.39);
+    start->rotation().setIdentity();
 
     base::ScopedState<base::SE3StateManifold> goal(start);
-    goal->setX(91.07);
-    goal->setY(17.98);
-    goal->setZ(328.01);
+    start->setX(17.49);
+    start->setY(10.12);
+    start->setZ(-40.39);
     goal->rotation().setIdentity();
         
     setup.setStartAndGoalStates(start, goal);
-    setup.getSpaceInformation()->setStateValidityCheckingResolution(0.05);
+    setup.getSpaceInformation()->setStateValidityCheckingResolution(0.005);
     setup.setup();
     
     setup.print();
