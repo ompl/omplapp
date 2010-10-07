1fd59666e22a138901b84a8b36110fd227d435b3
Ioan Sucan <isucan@gmail.com>
2010-10-07 19:58:36 +0000

    benchmark for SE3
    --HG--
    extra : convert_revision : 2fb270d232090f64049e67f706d443bd8872f4f6

--- trunk/omplapp/app/SE3RigidBodyPlanning/SE3RigidBodyPlanningBenchmark.cpp
+++ trunk/omplapp/app/SE3RigidBodyPlanning/SE3RigidBodyPlanningBenchmark.cpp
@@ -1,16 +1,18 @@
 #include "common/config.h"
 #include "common/SE3RigidBodyPlanning.h"
+#include <ompl/geometric/Benchmark.h>
+#include <ompl/geometric/planners/rrt/RRTConnect.h>
+#include <ompl/geometric/planners/kpiece/LBKPIECE1.h>
 using namespace ompl;
 
 int main()
 {
     app::SE3RigidBodyPlanning setup;
-    std::string robot_fname = std::string(OMPL_RESOURCE_DIR) + "/alpha_robot.dae";
-    std::string env_fname = std::string(OMPL_RESOURCE_DIR) + "/alpha_env.dae";
+    std::string robot_fname = std::string(OMPL_RESOURCE_DIR) + "/cubicles_robot.dae";
+    std::string env_fname = std::string(OMPL_RESOURCE_DIR) + "/cubicles_env.dae";
     setup.setRobotMesh(robot_fname.c_str());
     setup.setEnvironmentMesh(env_fname.c_str());
-
-
+    
     base::ScopedState<base::SE3StateManifold> start(setup.getSpaceInformation());
     start->setX(-4.96);
     start->setY(70.57);
@@ -22,17 +24,16 @@ int main()
     goal->setY(70.57);
     goal->setZ(40.62);
     goal->rotation().setIdentity();
-
+        
     setup.setStartAndGoalStates(start, goal);
     setup.getSpaceInformation()->setStateValidityCheckingResolution(0.01);
-    setup.setup();
     
-    setup.print();
-    if (setup.solve(60))
-    {
-	setup.simplifySolution();
-	setup.getSolutionPath().print(std::cout);
-    }
+    geometric::Benchmark b(setup);
+    b.addPlanner(base::PlannerPtr(new geometric::RRTConnect(setup.getSpaceInformation())));
+    b.addPlanner(base::PlannerPtr(new geometric::LBKPIECE1(setup.getSpaceInformation())));
     
+    b.benchmark(10, 1, 50);
+    b.saveResultsToStream();
+
     return 0;
 }
