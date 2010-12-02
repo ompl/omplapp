312db2e111d1261f75dc4e0f4ec89a216dd4e9e0
Mark Moll <mmoll@rice.edu>
2010-12-02 16:13:25 +0000

    build fixes, clean up OMPLAPP references
    --HG--
    extra : convert_revision : eba5686a02dc72b4630bf0b9e119435c09bafe3b

--- trunk/omplapp/CMakeModules/UseDrawstuff.cmake
+++ trunk/omplapp/CMakeModules/UseDrawstuff.cmake
@@ -1,4 +1,4 @@
-find_package(Drawstuff)
+find_package(Drawstuff QUIET)
 
 if( ( DRAWSTUFF_LIBRARY AND DRAWSTUFF_INCLUDE_DIR ) OR MSVC_IDE)
     # if already installed, show that library and headers were found
@@ -11,9 +11,10 @@ else( ( DRAWSTUFF_LIBRARY AND DRAWSTUFF_INCLUDE_DIR ) OR MSVC_IDE)
         DOWNLOAD_DIR "${CMAKE_SOURCE_DIR}/src/external"
         SVN_REPOSITORY "https://opende.svn.sourceforge.net/svnroot/opende/trunk"
         SVN_REVISION "-r1770"
-        URL_MD5 "f710e24a62db763d61d08667439d46fd"
         CONFIGURE_COMMAND "${CMAKE_BINARY_DIR}/drawstuff-prefix/src/drawstuff/configure" 
             "--enable-double-precision" "--with-pic"
+        UPDATE_COMMAND ""
+        BUILD_IN_SOURCE 1
         INSTALL_COMMAND "")
     # use a CMakeLists.txt file to configure build of PQP
     ExternalProject_Add_Step(drawstuff autogen
@@ -23,9 +24,22 @@ else( ( DRAWSTUFF_LIBRARY AND DRAWSTUFF_INCLUDE_DIR ) OR MSVC_IDE)
         WORKING_DIRECTORY "${CMAKE_BINARY_DIR}/drawstuff-prefix/src/drawstuff")
     
     # set the library and include variables
-    set(DRAWSTUFF_LIBRARY "${CMAKE_BINARY_DIR}/drawstuff-prefix/src/drawstuff-build/drawstuff/src/.libs/libdrawstuff.a" 
+    set(DRAWSTUFF_LIBRARY "${CMAKE_BINARY_DIR}/drawstuff-prefix/src/drawstuff/drawstuff/src/.libs/libdrawstuff.a" 
         CACHE FILEPATH "Location of ODE's drawstuff library" FORCE)
     set(DRAWSTUFF_INCLUDE_DIR "${CMAKE_BINARY_DIR}/drawstuff-prefix/src/drawstuff/include"
         CACHE PATH "Location of ODE's drawstuff header files" FORCE)
     set(DRAWSTUFF_FOUND TRUE)
+    
+    # since we will download and build ODE, we might as well use the 
+    # ODE library and header files *unless* another version of ODE has
+    # already been found.
+    if(NOT ODE_FOUND)
+        set(ODE_LIBRARY "${CMAKE_BINARY_DIR}/drawstuff-prefix/src/drawstuff/ode/src/.libs/libode.a" 
+            CACHE FILEPATH "Location of ODE library" FORCE)
+        set(ODE_INCLUDE_DIR "${CMAKE_BINARY_DIR}/drawstuff-prefix/src/drawstuff/include"
+            CACHE PATH "Location of ODE header files" FORCE)
+        set(ODE_DEFINITIONS "-DdDOUBLE")
+        set(ODE_VERSION "svn-r1770")
+        set(ODE_FOUND TRUE)        
+    endif(NOT ODE_FOUND)
 endif( ( DRAWSTUFF_LIBRARY AND DRAWSTUFF_INCLUDE_DIR ) OR MSVC_IDE)
