e8dee8592c6233de456d2478fab414c80335db53
Ioan Sucan <isucan@gmail.com>
2010-12-11 02:18:32 +0000

    update make website script
    --HG--
    extra : convert_revision : 3bdc49a5c0e3d5b7fe9ae5c0824bc1cd98559d0f

--- trunk/omplapp/doc/mkwebdocs.sh
+++ trunk/omplapp/doc/mkwebdocs.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 
+BRANCH=trunk
+
 # web server
 export SERVER=arachne.cs.rice.edu
 # directory on web server where different versions of the web site 
 # and other assets are located.
-export ASSETS_ROOT=/mnt/data2/ompl
-export ASSET_DIR=beta
+export ASSETS_ROOT=/mnt/data2/ompl/$BRANCH
+export ASSET_DIR=omplapp
+
+echo "Exporting OMPL.app documentation for $BRANCH"
 
 rm -rf ${ASSET_DIR}
 # copy all assets to the ASSET_DIR directory
@@ -15,8 +19,11 @@ for f in html/*.html; do
 done
 cp -r css js images html/*.png html/*.map html/search ${ASSET_DIR}
 
+# add symlink to OMPL
+cd $ASSET_DIR && ln -s ../ompl core && cd ..
+
 # copy everything to web server and fix permissions
-tar cf - --exclude .svn ${ASSET_DIR} | ssh ${SERVER} 'cd '${ASSETS_ROOT}'; tar xf -; chmod -R a+rX '${ASSET_DIR}'; chgrp -R ompl '${ASSET_DIR}'; chmod -R g+w '${ASSET_DIR}
+tar cf - --exclude .svn ${ASSET_DIR} | ssh ${SERVER} 'mkdir -p '${ASSETS_ROOT}'; cd '${ASSETS_ROOT}'; tar xf -; chmod -R a+rX '${ASSET_DIR}'; chgrp -R ompl '${ASSET_DIR}'; chmod -R g+w '${ASSET_DIR}
 
 # clean up
 rm -rf ${ASSET_DIR}
