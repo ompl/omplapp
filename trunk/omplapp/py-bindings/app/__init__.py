2d658e311ee289eb8e7dbd685d62e8fa34bff8f8
Mark Moll <mmoll@rice.edu>
2010-12-12 16:46:44 +0000

    fix licenses
    --HG--
    extra : convert_revision : 9ec5836de7fb2b198a22a5c2609778edfb6936fb

--- trunk/omplapp/py-bindings/app/__init__.py
+++ trunk/omplapp/py-bindings/app/__init__.py
@@ -1,3 +1,15 @@
+######################################################################
+# Rice University Software Distribution License
+#
+# Copyright (c) 2010, Rice University
+# All Rights Reserved.
+#
+# For a full description see the file named LICENSE.
+#
+######################################################################
+
+# Author: Mark Moll
+
 from os.path import abspath, dirname
 from ompl import dll_loader
 from ompl.geometric import *
