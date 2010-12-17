d23245beb5c4d299526e942ac3746b661f0ffc01
Ioan Sucan <isucan@gmail.com>
2010-12-17 16:45:45 +0000

    compiling assimp as static (if we need to compile assimp)
    --HG--
    extra : convert_revision : 53ee54420bee457a3931293342375ec192f256fa

--- trunk/omplapp/CMakeModules/UseAssimp.cmake
+++ trunk/omplapp/CMakeModules/UseAssimp.cmake
@@ -18,11 +18,12 @@ else(ASSIMP_LIBRARY AND ASSIMP_INCLUDE_DIR)
             "${CMAKE_BINARY_DIR}/assimp-prefix/src/assimp/code/CMakeLists.txt"
         CMAKE_ARGS 
             "-DCMAKE_INSTALL_PREFIX=${CMAKE_BINARY_DIR}/assimp-prefix"
+            "-DCMAKE_MODULE_PATH=${CMAKE_SOURCE_DIR}/ompl/CMakeModules"
             "-DCMAKE_BUILD_TYPE=Release" "-DCMAKE_BUILD_WITH_INSTALL_RPATH=ON"
             "-DCMAKE_INSTALL_NAME_DIR=${CMAKE_BINARY_DIR}/assimp-prefix/lib")
     
     # set the library and include variables
-    set(ASSIMP_LIBRARY "${CMAKE_BINARY_DIR}/assimp-prefix/lib/${CMAKE_SHARED_LIBRARY_PREFIX}assimp${CMAKE_SHARED_LIBRARY_SUFFIX}")
+    set(ASSIMP_LIBRARY "${CMAKE_BINARY_DIR}/assimp-prefix/lib/${CMAKE_STATIC_LIBRARY_PREFIX}assimp${CMAKE_STATIC_LIBRARY_SUFFIX}")
     if(EXISTS "${ASSIMP_LIBRARY}")
         set(ASSIMP_LIBRARY "${ASSIMP_LIBRARY}" CACHE FILEPATH "Location of 3D asset importer library" FORCE)
     endif()
