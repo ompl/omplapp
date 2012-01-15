c98ff9c9652263a95fc3dc81abf73995b191d7d6
Mark Moll <mmoll@rice.edu>
2012-01-14 23:15:33 -0600

    rename ODE to OpenDE everywhere to avoid confusion with ODESolver class
    --HG--
    branch : odeint
    rename : demos/OpenDEPlanning/ODEWorld.inc => demos/OpenDEPlanning/OpenDEWorld.inc
    rename : demos/OpenDEPlanning/displayODE.cpp => demos/OpenDEPlanning/displayOpenDE.cpp
    rename : demos/OpenDEPlanning/displayODE.h => demos/OpenDEPlanning/displayOpenDE.h
    rename : demos/OpenDEPlanning/planODE.cpp => demos/OpenDEPlanning/planOpenDE.cpp

--- CMakeModules/FindDrawstuff.cmake
+++ CMakeModules/FindDrawstuff.cmake
@@ -1,6 +1,6 @@
-# - Find Drawstuff (ODE Utility Lib)
+# - Find Drawstuff (OPENDE Utility Lib)
 
-# This module finds if the drawstuff library, part of Open Dynamics Engine (ODE).
+# This module finds if the drawstuff library, part of Open Dynamics Engine (OPENDE).
 #
 # The following variables are set:
 #  DRAWSTUFF_LIBRARY     = location of drawstuff library
@@ -8,21 +8,21 @@
 
 include(FindPackageHandleStandardArgs)
 
-find_package(ODE QUIET)
+find_package(OpenDE QUIET)
 
-if (ODE_FOUND)
+if (OPENDE_FOUND)
   # find the header file
-  find_path(DRAWSTUFF_INCLUDE_DIR drawstuff.h PATHS ${_ODE_INCLUDE_HINTS} ${ODE_INCLUDE_DIR}
+  find_path(DRAWSTUFF_INCLUDE_DIR drawstuff.h PATHS ${_OPENDE_INCLUDE_HINTS} ${OPENDE_INCLUDE_DIR}
       PATH_SUFFIXES drawstuff include/drawstuff
-       DOC "Location of ODE's drawstuff header files")
+       DOC "Location of OpenDE's drawstuff header files")
   if (DRAWSTUFF_INCLUDE_DIR)
     string(REGEX REPLACE "/drawstuff$" "" DRAWSTUFF_INCLUDE_DIR "${DRAWSTUFF_INCLUDE_DIR}")
   endif()
 
   # find the library
-  find_library(DRAWSTUFF_LIBRARY drawstuff PATHS ${_ODE_LIB_HINTS}
+  find_library(DRAWSTUFF_LIBRARY drawstuff PATHS ${_OPENDE_LIB_HINTS}
       PATH_SUFFIXES lib drawstuff/src/.libs src/.libs
-      DOC "Location of ODE's drawstuff library")
+      DOC "Location of OpenDE's drawstuff library")
 endif()
 
 find_package_handle_standard_args(Drawstuff DEFAULT_MSG DRAWSTUFF_INCLUDE_DIR DRAWSTUFF_LIBRARY)
