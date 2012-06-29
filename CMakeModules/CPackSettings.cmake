e42483833d3bedaf5120f32274c06a4d7d380a59
Mark Moll <mmoll@rice.edu>
2012-06-29 11:02:27 -0500

    add deb cpack settings

--- CMakeModules/CPackSettings.cmake
+++ CMakeModules/CPackSettings.cmake
@@ -33,7 +33,25 @@ set(CPACK_SOURCE_IGNORE_FILES
     "config.h$"
     ".tgz$")
 set(CPACK_SOURCE_GENERATOR "TGZ;ZIP")
-set(CPACK_GENERATOR "TGZ;ZIP")
+set(CPACK_GENERATOR "TGZ")
+
+# Run "cmake -DOMPL_INSTALL_DEPENDENCIES=ON" before "make package" to include
+# PQP, FCL, and CCD. The resulting .deb file is intended for (recent) Ubuntu
+# releases. The dependencies likely have different names on other Linux
+# distributions
+if(${CMAKE_SYSTEM_NAME} STREQUAL "Linux")
+  set(CPACK_GENERATOR "DEB;${CPACK_GENERATOR}")
+  set(CPACK_DEBIAN_PACKAGE_ARCHITECTURE "${CMAKE_SYSTEM_PROCESSOR}")
+  execute_process(COMMAND "/usr/bin/lsb_release" "-rs"
+      OUTPUT_VARIABLE UBUNTU_RELEASE
+      OUTPUT_STRIP_TRAILING_WHITESPACE)
+  set(CPACK_PACKAGE_FILE_NAME "omplapp_${OMPL_VERSION}_${CPACK_DEBIAN_PACKAGE_ARCHITECTURE}-Ubuntu${UBUNTU_RELEASE}")
+  set(CPACK_DEBIAN_PACKAGE_DEPENDS "python${PYTHON_VERSION}, libboost-all-dev, python-qt4-dev, python4-qt4-gl, freeglut3-dev, libode-dev, libassimp-dev, libflann-dev")
+endif()
+
+if(WIN32)
+  set(CPACK_GENERATOR "ZIP;${CPACK_GENERATOR}")
+endif()
 
 include(CPack)
 
