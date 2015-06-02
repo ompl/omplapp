aaa203af46524492d71be173ff148ce6ee773c9b
Prudhvi Boyapalli <prb2@rice.edu>
2015-06-02 11:38:31 -0500

    Cleaned up omplweb.py, starting point for redesigned 'solve()'.

--- omplweb.py
+++ omplweb.py
@@ -164,155 +164,13 @@ def parse(settings, env_path, robot_path):
 	return problem
 
 
-def convert_path_to_json(path, solved=True):
-	"""
-	Given a list of list states from ompl, generates a json string with the
-	following form. Will set status to didSolve.
-
-	status: true
-	path: [ [x,y,z,q1,q2,q3,q4], ... ]
-
-	status: true
-	0: ( (x,y,z), (angle,angle,angle,angle) ) //this is a quaternion
-	1: ( (x,y,z), (angle,angle,angle,angle) )
-	...
-
-	"""
-
-	solved_json = 'true' if solved else 'false'
-
-	# first, create an ordered dictionary (if possible)
-
-	# TODO: actually implement this
-
-	dictionary = {}
-	dictionary['solved'] = solved_json
-	dictionary['raw_path'] = str(path)
-
-	dictionary['path'] = path
-
-	#TODO: do string conversion here if possible - right now expects string path
-
-	#print 'warning: path is not a GeometricPath, only sending raw string'
-	#	 print pathlist[0]
-
-	return json.dumps(dictionary)
-
-
 def solve(problem):
 	"""
 	Given an instance of the Problem class, containing the problem configuration
 	data, solves the motion planning problem and returns either the solution
 	path or a failure message.
-
-	# Old code # NOTE: not very efficient, but I do not want any statefulness on server side
 	"""
-
-	# load problem information from Django.
-	#problem = Problem.objects.get(pk=problem_id)
-	# if problem == None:
-		# print 'ompl.solve: problem failed to load'
-		# return (False, '')
-
-	# Initialize with the values in problem
-	# Old code #  NOTE: just do super easy rigid body in 3D, SE(3) for now
-	print 'OMPL:	Setting up problem'
-
-	# space = ob.SE3StateSpace()
-	# omplSetup = og.SimpleSetup(space)
-	ompl_setup = oa.SE3RigidBodyPlanning()
-
-	#TODO: set validity checker? how did OMPLApp handle this?
-
-	# Set robot and environment meshes
-	print "OMPL:	Setting robot mesh with: %s" % str(problem.robot_path)
-	ompl_setup.setRobotMesh(str(problem.robot_path))
-	print "OMPL:	Setting environment mesh with: %s" % str(problem.env_path)
-	ompl_setup.setEnvironmentMesh(str(problem.env_path))
-
-	# Set start pose
-	start = ob.State(ompl_setup.getGeometricComponentStateSpace())
-	start().setX(float(problem.start_x))
-	start().setY(float(problem.start_y))
-	start().setZ(float(problem.start_z))
-
-	# Use Axis-Angle description to set axis and angle
-	start().rotation().setAxisAngle(
-			float(problem.start_axis_x), float(problem.start_axis_y),
-			float(problem.start_axis_z), float(problem.start_theta))
-
-	# Set goal pose
-	goal = ob.State(ompl_setup.getGeometricComponentStateSpace())
-	goal().setX(float(problem.goal_x))
-	goal().setY(float(problem.goal_y))
-	goal().setZ(float(problem.goal_z))
-
-	# Use Axis-Angle description to set axis and angle
-	goal().rotation().setAxisAngle(
-			float(problem.goal_axis_x), float(problem.goal_axis_y),
-			float(problem.goal_axis_z), float(problem.goal_theta))
-
-	ompl_setup.setStartAndGoalStates(start, goal)
-
-	# Set bounds
-	bounds = ob.RealVectorBounds(3)
-	bounds.low[0] = float(problem.bounds_min_x)
-	bounds.low[1] = float(problem.bounds_min_y)
-	bounds.low[2] = float(problem.bounds_min_z)
-	bounds.high[0] = float(problem.bounds_max_x)
-	bounds.high[1] = float(problem.bounds_max_y)
-	bounds.high[2] = float(problem.bounds_max_z)
-	ompl_setup.getGeometricComponentStateSpace().setBounds(bounds)
-
-
-	# grab planner
-	#TODO: grab this from the user's preference, but for now use the first planner in problem's list
-	#TODO: um get a planner and use it instead of the default!
-
-
-	# Solve
-	did_solve = ompl_setup.solve(float(problem.time_limit))
-	path_string = ''
-
-	states_list = []
-	if did_solve:
-		path = ompl_setup.getSolutionPath()
-		did_solve = path.check()
-		if did_solve == False:
-			print("OMPL: 	Path reported by planner seems to be invalid!")
-			return "Solution path was invalid. Try again."
-		else:
-			print "OMPL: 	Solve seems successful"
-
-			# convert to json string
-			num_states = path.getStateCount()
-			path_list = [ompl_setup.getGeometricComponentState(ob.State(
-				ompl_setup.getGeometricComponentStateSpace(), path.getState(i)), 0)
-				for i in range(num_states)]
-
-			#BIG TODO: PROPERLY access states from ompl.
-			# For states that don't let us access them like this WILL segfault
-			for state in path_list:
-				state_list = [
-							str(state[0]),
-							str(state[1]),
-							str(state[2]),
-							str(state[3]),
-							str(state[4]),
-							str(state[5]),
-							str(state[6])
-							]
-				states_list.append(state_list)
-
-			print 'OMPL:	Successful state to path (despite segfault risk here)'
-
-			path_string = convert_path_to_json(states_list,did_solve)
-
-	# return "DEBUGGING"
-	return path_string
-
-
-	# TODO: return path properly, right now just return it all and the status as a string
+	print problem
 
 
 ########## Flask Code ##########
@@ -389,27 +247,7 @@ def upload():
 
 @app.route("/")
 def index():
-	return flask.render_template("index.html")
-
-
-@app.route("/<name>")
-def hello(name):
-	return flask.render_template("name.html", name=name)
-
-
-@app.route("/webgl")
-def viewWebGl():
-	return flask.render_template("viewWebGL.html")
-
-
-@app.route("/simple")
-def simpleGL():
-	return flask.render_template("simpleGL.html")
-
-
-@app.route("/<usrname>/<int:uid>")
-def printName(usrname, uid):
-	return "Name: %s, ID: %d" % (usrname, uid)
+	return flask.redirect(flask.url_for('omplapp'))
 
 
 if __name__ == "__main__":
