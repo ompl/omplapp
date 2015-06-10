31f3cf6beab720595f37555c94a010ea79fe2861
Prudhvi Boyapalli <prrb02@gmail.com>
2015-06-10 16:14:30 -0500

    UI upgrade is complete. Configuration page is complete. Visualization and benchmarking pages are currently place holders.

--- static/scripts/omplweb.js
+++ static/scripts/omplweb.js
@@ -1,105 +1,7 @@
 
-var asdf_planners= {
-	"BKPIECE1" : {
-		"Border fraction" : 0.90,
-		"Range" : 0.0
-	},
-	"EST" : {
-		"Goal bias" : 0.05,
-		"Range" : 0.0
-	},
-	"FMT" : {
-		"Free space volume" : 1.0,
-		"Num samples" : 1000.0,
-		"Radius multiplier" : 1.10
-	},
-	"KPIECE1" : {
-		"Border fraction" : 0.90,
-		"Goal bias" : 0.05,
-		"Range" : 0.0
-	},
-	"LBKPIECE1" : {
-		"Border fraction" : 0.90,
-		"Range" : 0.0
-	},
-	"LBTRRT" : {
-		"Epsilon" : 0.40,
-		"Goal bias" : 0.05,
-		"Range" : 0.0
-	},
-	"LazyPRM" : {
-		"Max nearest neighbors" : 8,
-		"Range" : 0.0
-	},
-	"LazyPRMstar" : {
-		"Max nearest neighbors" : 8,
-		"Range" : 0
-	},
-	"LazyRRT" : {
-		"Goal bias" : 0.05,
-		"Range" : 0.0
-	},
-	"PDST" : {
-		"Goal bias" : 0.05,
-	},
-	"PRM" : {
-		"Max nearest neighbors" : 8,
-	},
-	"PRMstar" : {
-		"Max nearest neighbors" : 8,
-	},
-	"RRT" : {
-		"Goal bias" : 0.05,
-		"Range" : 0.0
-	},
-	"RRTConnect" : {
-		"Range" : 0.0
-	},
-	"RRTstar" : {
-		"Delay collision checking" : "False",
-		"Goal bias" : 0.05,
-		"Prune" : "False",
-		"Range" : 0.0
-	},
-	"SBL" : {
-		"Range" : 0.0
-	},
-	"SPARS" : {
-		"Dense delta fraction" : 0.0010,
-		"Max failures" : 1000.0,
-		"Sparse delta fraction" : 0.25,
-		"Stretch factor" : 3.0
-	},
-	"SPARStwo" : {
-		"Dense delta fraction" : 0.0010,
-		"Max failures" : 3000.0,
-		"Sparse delta fraction" : 0.25,
-		"Stretch factor" : 3.0
-
-	},
-	"STRIDE" : {
-		"Degree" : 16.0,
-		"Estimated dimension" : 1.0,
-		"Goal bias" : 0.05,
-		"Max degree" : 18.0,
-		"Max pts per leaf" : 6.0,
-		"Min degree" : 12.0,
-		"Min valid path fraction" : 0.20,
-		"Range" : 0.0,
-		"Use projected distance" : "False"
-	},
-	"TRRT" : {
-		"Goal bias" : 0.05,
-		"Max states failed" : 10.0,
-		"Range" : 0.0,
-		"Temp change factor" : 2.00
-	}
-}
-
 var planners = null;
 
 var results = "";
-
 
 $(document).ready(function() {
 	load_configuration();
@@ -153,7 +55,6 @@ function load_configuration () {
 	});
 }
 
-
 function load_visualization () {
 	$(".active_nav_item").removeClass('active_nav_item')
 
@@ -161,7 +62,6 @@ function load_visualization () {
 		$('#nav_viz').addClass('active_nav_item');
 	});
 }
-
 
 function load_benchmarking () {
 	$(".active_nav_item").removeClass('active_nav_item')
@@ -171,23 +71,22 @@ function load_benchmarking () {
 	});
 }
 
-
 function load_planner_params(planner_name) {
 	if (planners != null) {
 		var plannerConfigHTML = "";
-		plannerConfigHTML += "<div class='col-md-8'>";
-		plannerConfigHTML += "<h4>" + planner_name.split(".")[2] + " Parameters</h4>";
-		plannerConfigHTML += "<table class='table'>";
+		plannerConfigHTML += "<div class='pane-title'>" + planner_name.split(".")[2] + " Parameters</div>";
+		plannerConfigHTML += "<div class='container-fluid'>";
+		plannerConfigHTML += "<div class='col-md-12'>";
 
 		params = planners[planner_name]
 		for (var key in params) {
 			if (params.hasOwnProperty(key)) {
-				plannerConfigHTML += "<tr><td>" + params[key][0]  + "</td>";
-				plannerConfigHTML += "<td><input type='text' name='" + key +  "' class='form-field' value='" + params[key][3] + "'></td></tr>";
+				plannerConfigHTML += params[key][0];
+				plannerConfigHTML += "<input type='text' name='" + key +  "' class='form-field form-control' value='" + params[key][3] + "'><br><br>";
 			}
 		}
 
-		plannerConfigHTML += "</table>";
+		plannerConfigHTML += "</div>";
 		plannerConfigHTML += "</div>";
 
 		$("#plannerPane").html(plannerConfigHTML);
@@ -228,12 +127,133 @@ function showPane(paneID) {
 	$(linkID).addClass('sidebar-active');
 }
 
+
+function loadConfig() {
+	var cfgFile = $("#config")[0].files[0];
+
+	if (cfgFile != null) {
+		var reader = new FileReader();
+
+		reader.readAsText(cfgFile);
+
+		reader.onload = function () {
+			var cfgData = parseConfig(reader.result);
+
+			//TODO: Make this neater
+			document.getElementsByName("name")[0].value = cfgData['name'];
+			// document.getElementsByName("planners")[0].value = "";
+			document.getElementsByName("start.x")[0].value = cfgData['start.x'];
+			document.getElementsByName("start.y")[0].value = cfgData['start.y'];
+			document.getElementsByName("start.z")[0].value = cfgData['start.z'];
+			document.getElementsByName("start.theta")[0].value = cfgData['start.theta'];
+			document.getElementsByName("start.axis.x")[0].value = cfgData['start.axis.x'];
+			document.getElementsByName("start.axis.y")[0].value = cfgData['start.axis.y'];
+			document.getElementsByName("start.axis.z")[0].value = cfgData['start.axis.z'];
+			document.getElementsByName("goal.x")[0].value = cfgData['goal.x'];
+			document.getElementsByName("goal.y")[0].value = cfgData['goal.y'];
+			document.getElementsByName("goal.z")[0].value = cfgData['goal.z'];
+			document.getElementsByName("goal.theta")[0].value = cfgData['goal.theta'];
+			document.getElementsByName("goal.axis.x")[0].value = cfgData['goal.axis.x'];
+			document.getElementsByName("goal.axis.y")[0].value = cfgData['goal.axis.y'];
+			document.getElementsByName("goal.axis.z")[0].value = cfgData['goal.axis.z'];
+			document.getElementsByName("bounds.min.x")[0].value = cfgData['volume.min.x'];
+			document.getElementsByName("bounds.min.y")[0].value = cfgData['volume.min.y'];
+			document.getElementsByName("bounds.min.z")[0].value = cfgData['volume.min.z'];
+			document.getElementsByName("bounds.max.x")[0].value = cfgData['volume.max.x'];
+			document.getElementsByName("bounds.max.y")[0].value = cfgData['volume.max.y'];
+			document.getElementsByName("bounds.max.z")[0].value = cfgData['volume.max.z'];
+			document.getElementsByName("time_limit")[0].value = cfgData['time_limit'];
+			document.getElementsByName("mem_limit")[0].value = cfgData['mem_limit'];
+			document.getElementsByName("run_count")[0].value = cfgData['run_count'];
+
+		}
+	} else {
+		alert("Please select a valid configuration file.")
+	}
+}
+
+function parseConfig(cfgText) {
+	var cfgData = {};
+
+	// Separate into lines
+	var cfgLines = cfgText.split("\n");
+
+	for (var i=0; i < cfgLines.length; i++) {
+		if(cfgLines[i][0] != "[") {
+			// Remove all extra spacing
+			var line = cfgLines[i].replace(/\s+/g, '');
+
+			// Split into (key, value) pairs
+			var items = line.split("=");
+			cfgData[items[0]]= items[1];
+		} else {
+			// This config line isn't used
+			// console.log("Ignored: ", cfgLines[i]);
+		}
+	}
+
+	return cfgData;
+}
+
+function clearAllFields() {
+	if (confirm("Are you sure you want to clear all fields?")) {
+		$('.form-field').each(function () {
+			$(this).val('');
+			$(this).css("background-color", "white");
+		});
+		$('#config').val('');
+		$('#results').html('');
+		results = "";
+		showPane('#robotPane');
+	}
+	
+}
+
+function validateFields() {
+	// Check that a problem has been selected
+	if ($('#problems').val() != 'custom' && $('#planners').val() != null) {
+		return true;
+	}
+
+	// Check that a planner has been selected
+	if ($('#planners').val() == null) {
+		alert("Please select a planner from the drop down menu.");
+		return false;
+	}
+
+	// Check that a planner has been selected
+	if ($('#problems').val() == null && $('#planners').val() == null) {
+		alert("Please select a problem and planner from the drop down menus.");
+		return false;
+	}
+
+	// Check if the user uploaded a custom problem
+	if ($('#problems').val() == 'custom' && $('#planners').val() != null) {
+		var valid = true;
+
+		// Ensure that the all the needed fields for a problem have values
+		$('.form-field').each(function () {
+			if ($(this).val() === '') {
+				valid = false;
+
+				// Highlight the incorrect field
+				$(this).css("background-color", "#fda9a0");
+			} else {
+				$(this).css("background-color", "white");
+			}
+		});
+		return valid;
+	}
+
+}
+
+
 function solve(){
 	// Check that all fields are filled in
 	var validConfig = validateFields();
 	if (validConfig == true) {
-		var html = "";
-		html += "<h2><center>Results</h2>"
+		var html = "<br><br>";
+		html += "<div class='pane-title'>Results</div>"
 
 		// Bring up the loading screen
 		$.blockUI({
@@ -295,123 +315,4 @@ function solve(){
 	} else {
 		// Invalid fields have been highlighted by 'validateField()'.
 	}
-}
-
-
-function loadConfig() {
-	var cfgFile = $("#config")[0].files[0];
-
-	if (cfgFile != null) {
-		var reader = new FileReader();
-
-		reader.readAsText(cfgFile);
-
-		reader.onload = function () {
-			var cfgData = parseConfig(reader.result);
-
-			//TODO: Make this neater
-			document.getElementsByName("name")[0].value = cfgData['name'];
-			// document.getElementsByName("planners")[0].value = "";
-			document.getElementsByName("start.x")[0].value = cfgData['start.x'];
-			document.getElementsByName("start.y")[0].value = cfgData['start.y'];
-			document.getElementsByName("start.z")[0].value = cfgData['start.z'];
-			document.getElementsByName("start.theta")[0].value = cfgData['start.theta'];
-			document.getElementsByName("start.axis.x")[0].value = cfgData['start.axis.x'];
-			document.getElementsByName("start.axis.y")[0].value = cfgData['start.axis.y'];
-			document.getElementsByName("start.axis.z")[0].value = cfgData['start.axis.z'];
-			document.getElementsByName("goal.x")[0].value = cfgData['goal.x'];
-			document.getElementsByName("goal.y")[0].value = cfgData['goal.y'];
-			document.getElementsByName("goal.z")[0].value = cfgData['goal.z'];
-			document.getElementsByName("goal.theta")[0].value = cfgData['goal.theta'];
-			document.getElementsByName("goal.axis.x")[0].value = cfgData['goal.axis.x'];
-			document.getElementsByName("goal.axis.y")[0].value = cfgData['goal.axis.y'];
-			document.getElementsByName("goal.axis.z")[0].value = cfgData['goal.axis.z'];
-			document.getElementsByName("bounds.min.x")[0].value = cfgData['volume.min.x'];
-			document.getElementsByName("bounds.min.y")[0].value = cfgData['volume.min.y'];
-			document.getElementsByName("bounds.min.z")[0].value = cfgData['volume.min.z'];
-			document.getElementsByName("bounds.max.x")[0].value = cfgData['volume.max.x'];
-			document.getElementsByName("bounds.max.y")[0].value = cfgData['volume.max.y'];
-			document.getElementsByName("bounds.max.z")[0].value = cfgData['volume.max.z'];
-			document.getElementsByName("time_limit")[0].value = cfgData['time_limit'];
-			document.getElementsByName("mem_limit")[0].value = cfgData['mem_limit'];
-			document.getElementsByName("run_count")[0].value = cfgData['run_count'];
-
-		}
-	} else {
-		alert("Please select a valid configuration file.")
-	}
-}
-
-
-function parseConfig(cfgText) {
-	var cfgData = {};
-
-	// Separate into lines
-	var cfgLines = cfgText.split("\n");
-
-	for (var i=0; i < cfgLines.length; i++) {
-		if(cfgLines[i][0] != "[") {
-			// Remove all extra spacing
-			var line = cfgLines[i].replace(/\s+/g, '');
-
-			// Split into (key, value) pairs
-			var items = line.split("=");
-			cfgData[items[0]]= items[1];
-		} else {
-			// This config line isn't used
-			// console.log("Ignored: ", cfgLines[i]);
-		}
-	}
-
-	return cfgData;
-}
-
-
-function clearAllFields() {
-	$('.form-field').each(function () {
-		$(this).val('');
-		$(this).css("background-color", "white");
-	});
-	$('#config').val('');
-	$('#results').html('');
-	showPane('#robotPane');
-}
-
-
-function validateFields() {
-	// Check that a problem has been selected
-	if ($('#problems').val() != 'custom' && $('#planners').val() != null) {
-		return true;
-	}
-
-	// Check that a planner has been selected
-	if ($('#planners').val() == null) {
-		alert("Please select a planner from the drop down menu.");
-		return false;
-	}
-
-	// Check that a planner has been selected
-	if ($('#problems').val() == null && $('#planners').val() == null) {
-		alert("Please select a problem and planner from the drop down menus.");
-		return false;
-	}
-
-	// Check if the user uploaded a custom problem
-	if ($('#problems').val() == 'custom' && $('#planners').val() != null) {
-		var valid = true;
-
-		// Ensure that the all the needed fields for a problem have values
-		$('.form-field').each(function () {
-			if ($(this).val() === '') {
-				valid = false;
-
-				// Highlight the incorrect field
-				$(this).css("background-color", "#fda9a0");
-			} else {
-				$(this).css("background-color", "white");
-			}
-		});
-		return valid;
-	}
-
 }
