32f108f549d2e264200ea309b5eef5d7eb35fea5
Prudhvi Boyapalli <prrb02@gmail.com>
2015-06-25 16:52:18 -0500

    The configured planner data is available in benchmarking

--- static/scripts/benchmarking.js
+++ static/scripts/benchmarking.js
@@ -1,4 +1,4 @@
-
+var config;
 
 
 
@@ -11,17 +11,40 @@
  * @param 	None
  * @return 	None
  */
-function load_benchmarking_page () {
+function load_benchmarking_page() {
 	// TODO: Save all of the config info
+	if (solutionData == null) {
+		alert("Configure and solve a problem before benchmarking.");
+	} else {
+		// config = getProblemConfig();
+		var defaultPlanner = getConfiguredPlanner();
 
+		console.log(defaultPlanner);
 
-	// Unhighlight the old active tab
-	$(".active_nav_item").removeClass('active_nav_item');
+		// Unhighlight the old active tab
+		$(".active_nav_item").removeClass('active_nav_item');
 
-	// Load the HTML for the configuration settings
-	$("#content").load("omplapp/components/benchmarking", function () {
-		// Make config the active tab
-		$('#nav_bench').addClass('active_nav_item');
+		// Load the HTML for the configuration settings
+		$("#content").load("omplapp/components/benchmarking", function () {
+			// Make config the active tab
+			$('#nav_bench').addClass('active_nav_item');
 
-	});
+			addPlanner(defaultPlanner['name'], defaultPlanner['parameters']);
+		});
+	}
+}
+
+function addPlanner(name, params) {
+	
+	var planner = "";
+	planner += "<tr>";
+	planner += "<th>" + name + "</th>";
+	planner += "<td><table class='table-condensed' id='defaultPlannerParams'>"
+			
+	for (var param in params) {
+		planner += "<tr><td>" + param + "</td><td>" + params[param] + "</td></tr>";
+	}
+
+	planner+= "</table></td></tr>";
+	$('#planners').append(planner);
 }
