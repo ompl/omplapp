3a0e868b708aadc90e769ed6c5f128cbe253ff03
Prudhvi Boyapalli <prrb02@gmail.com>
2015-07-07 10:32:30 -0500

    Added ability to remove planners. Fixed cfg bug. Adjusted link styles.

--- static/scripts/benchmarking.js
+++ static/scripts/benchmarking.js
@@ -2,6 +2,7 @@
 
 var benchmarkPlanners = {};
 var firstTime = true;
+var plannerCounter = 0;
 
 /**
  * Loads benchmarking components.
@@ -30,7 +31,14 @@ function createDefaultPlannerEntry () {
 	var params = defaultPlanner['parameters'];
 	var numParams = Object.keys(params).length + 1;
 
-	var planner = "<tr><th colspan='3' rowspan='" + numParams + "' class='planner-name'>" + name.split(".")[2] + "</th></tr>";
+	var planner = "<table class='table planner-table table-condensed' id='" + plannerCounter + "'>";
+	// planner += "<a class='remove'><span class='glyphicon glyphicon-remove'></a>";
+	// planner += "<caption class='planner-name'>" + name.split(".")[2];
+	// planner += "</caption>";
+
+	planner += "<tr><th>" + name.split(".")[2] + "</th>";
+	planner += "<th><a class='remove' onclick='removePlanner(" + plannerCounter  + ")'><span class='glyphicon glyphicon-remove'></a></th></tr>";
+	plannerCounter += 1;
 
 	for (var param in params) {
 		planner += "<tr><td class='planner-param'>" + planners[name][param][0] + "</td>";
@@ -38,7 +46,8 @@ function createDefaultPlannerEntry () {
 
 	}
 
-	$('#planner-entries').append(planner);
+	planner += "</table>";
+	$('#planner-tables').append(planner);
 }
 
 
@@ -51,21 +60,15 @@ function createDefaultPlannerEntry () {
  */
 function addPlanner (name) {
 
-	createPlannerEntry(name);
-
-}
-
-
-function createPlannerEntry(name) {
 	var params = planners[name]
 	var numParams = Object.keys(params).length + 1;
-	var planner = "";
 	var name = name.split(".")[2];
 
-	planner += "<tr><th colspan='3' rowspan='" + numParams + "' class='planner-name'>";
-	planner += name;
-	planner += "</th></tr>";
+	var planner = "<table class='table planner-table table-condensed' id='" + plannerCounter + "'>";
 
+	planner += "<tr><th>" + name + "</th>";
+	planner += "<th><a class='remove' onclick='removePlanner(" + plannerCounter  + ")'><span class='glyphicon glyphicon-remove'></a></th></tr>";
+	plannerCounter += 1;
 
 	for (var key in params) {
 		if (params.hasOwnProperty(key)) {
@@ -75,8 +78,16 @@ function createPlannerEntry(name) {
 		}
 	}
 
-	$('#planner-entries').append(planner);
+	planner += "</table>";
+	$('#planner-tables').append(planner);
 
+}
+
+/**
+ *
+ */
+function removePlanner (planner) {
+	$('table').remove('#' + planner);
 }
 
 
@@ -89,16 +100,20 @@ function createPlannerEntry(name) {
 function getBenchmarkingPlanners() {
 	var benchPlanners = "";
 	var currentPlanner = "";
+	var modifiedPlannerName = "";
 
-	var inputs = $('#planner-entries input');
+	var inputs = $('#planner-tables input');
 	for (var i = 0; i < inputs.length; i++) {
+		modifiedPlannerName = inputs[i].name.toLowerCase().replace("1", "").replace("two", "2");
+
 		if (currentPlanner != inputs[i].name) {
-			benchPlanners += inputs[i].name.toLowerCase() + "=\n";
+			benchPlanners +=  modifiedPlannerName + "=\n";
 		}
 		currentPlanner = inputs[i].name;
 
-		benchPlanners += inputs[i].name.toLowerCase() + "." + inputs[i].id + "=" + inputs[i].value + "\n";
+		benchPlanners += modifiedPlannerName + "." + inputs[i].id + "=" + inputs[i].value + "\n";
 	}
+	console.log(benchPlanners);
 	return benchPlanners;
 }
 
