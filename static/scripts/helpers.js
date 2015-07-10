4522718e2eb74f86816834103636255ef42308d7
Prudhvi Boyapalli <prrb02@gmail.com>
2015-07-10 11:47:16 -0500

    Added keyboard shortcuts

--- static/scripts/helpers.js
+++ static/scripts/helpers.js
@@ -13,3 +13,44 @@ function showAlert(page, type, msg) {
 	var area = "#" + page + "-alerts";
 	$(area).html(alert);
 }
+
+
+/* Keyboard Shortcuts */
+
+Mousetrap.bind('ctrl+c', function(e) {
+	$("#configure-problem-page").click();
+});
+
+Mousetrap.bind('ctrl+b', function(e) {
+	$("#benchmarking-page").click();
+});
+
+Mousetrap.bind('ctrl+s', function(e) {
+	$("#configure-problem-page").click();
+	$("#solve-tab").click();
+	$("#solve-button").click();
+});
+
+Mousetrap.bind('ctrl+e', function(e) {
+	if ($("#problems").val() == "custom") {
+		$("#configure-problem-page").click();
+		$("#problem-tab").click();
+		$("#env_path").click();
+	}
+});
+
+Mousetrap.bind('ctrl+r', function(e) {
+	if ($("#problems").val() == "custom") {
+		$("#configure-problem-page").click();
+		$("#problem-tab").click();
+		$("#robot_path").click();
+	}
+});
+
+Mousetrap.bind('ctrl+o', function(e) {
+	if ($("#problems").val() == "custom") {
+		$("#configure-problem-page").click();
+		$("#problem-tab").click();
+		$("#config").click();
+	}
+});
