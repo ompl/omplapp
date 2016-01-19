2664c330b4513e36f700b7fa22a081e1ba102271
Mark Moll <mmoll@rice.edu>
2016-01-19 12:45:53 -0600

    Simplify dependency checks of fcl, libccd, and assimp. No more support for automatically downloading and installing dependencies. They are available through package managers on recent versions of Ubuntu and OS X.

---
+++ CMakeModules/FindFCL.cmake
@@ -0,0 +1,6 @@
+include(FindPackageHandleStandardArgs)
+find_package(PkgConfig)
+if(PKGCONFIG_FOUND)
+    pkg_check_modules(FCL fcl>=0.3.1)
+endif()
+find_package_handle_standard_args(FCL DEFAULT_MSG FCL_LIBRARIES FCL_INCLUDE_DIRS)
